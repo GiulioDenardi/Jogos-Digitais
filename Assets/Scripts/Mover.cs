2f540d75aedc0a14b1572d21f2fc797621dd878f
Giulio Denardi <giulio.denardi@buscapecompany.com>
2017-04-09 19:22:38 -0300

    Colocando mais alguns ans assets, e fazendo bolt (problema com script - dificuldade com linux)

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    public float speed;
+
+    void Start () {
+        rigidbody.velocity = transform.forward * speed;
+    }
+}
